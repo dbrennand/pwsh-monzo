987c331d3e428abe265d0eaa752a087a3126a2c5
Dextroz <25749668+Dextroz@users.noreply.github.com>
2019-10-05 12:55:49 +0100

    Add New-MonzoAuthorisationCode

---
+++ pwsh-monzo/Public/New-MonzoAuthorisationCode.ps1
@@ -0,0 +1,139 @@
+function New-MonzoAuthorisationCode {
+    <#
+    .SYNOPSIS
+        Obtain a new authorisation code from the Monzo API.
+    
+    .DESCRIPTION
+        Obtain a new authorisation code from the Monzo API which can be exchanged for an access token.
+    
+    .PARAMETER MonzoApplication
+        A Monzo application - created using New-MonzoApplication.
+    
+    .PARAMETER Email
+        The email address used during the authentication process to retrieve the authorisation code.
+    
+    .EXAMPLE
+        $MonzoApplication = New-MonzoApplication -Name "MyMonzoApp" -ClientCredential $Credentials -RedirectURI "https://foobar.com/oauth/callback" -Guid $MyGuid
+        $AuthorisationCode = $MonzoApplication | New-MonzoAuthorisationCode -Email "foobar@somemail.com"
+    
+    .EXAMPLE
+        $MonzoApplication = New-MonzoApplication -Name "MyMonzoApp" -ClientCredential $Credentials -RedirectURI "https://foobar.com/oauth/callback" -Guid $MyGuid
+        $AuthorisationCode = New-MonzoAuthorisationCode -MonzoApplication $MonzoApplication -Email "foobar@somemail.com"
+    #>
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    [OutputType("MonzoAPI.OAuth.AuthorisationCode")]
+    param (
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias("MonzoApp")]
+        [PSTypeName("MonzoAPI.Application")]
+        $MonzoApplication,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript( {
+                try {
+                    $null = [MailAddress]$_
+                }
+                catch {
+                    Write-Error -Message "The email string: $_ is not a valid email address." -ErrorAction "Stop"
+                }
+            })]
+        [String]
+        $Email
+    )
+    
+    begin {
+
+        # Before doing anything, check that the PSCustomObject contains ClientID and ClientSecret.
+        if (-not ($MonzoApplication.ClientCredential.UserName -and $MonzoApplication.ClientCredential.Password)) {
+            Write-Error -Message "ClientId and ClientSecret weren't present in MonzoAPI.Application." -ErrorAction "Stop"
+        }
+
+        # Check if Selenium is installed on the host.
+        if (Get-Module -ListAvailable -Name "Selenium") {
+            Import-Module -Name "Selenium"
+        }
+        else {
+            Write-Error -Message "Selenium isn't installed, attempting to install..."
+            try {
+                Install-Module -Name "Selenium" -RequiredVersion "2.0.0" -Force -AllowClobber -Verbose
+            }
+            catch {
+                Write-Error -Message "Selenium failed to install. Terminating..." -ErrorAction "Stop"
+            }
+        }
+    }
+    
+    process {
+        
+        try {
+            # Build url.
+            $Url = "https://auth.monzo.com/?
+            client_id=$($MonzoApplication.ClientCredential.UserName)
+            &redirect_uri=$($MonzoApplication.RedirectURI)
+            &response_type=code
+            &state=$($MonzoApplication.Guid.Guid)"
+            Write-Verbose -Message "The url is: $($Url)"
+            
+            # Automate browser using Selenium to accept the application.
+            $Driver = Start-SeFirefox
+            # Navigate to the url.
+            Enter-SeUrl -Url $Url -Driver $Driver
+
+            # Find and wait for the button element.
+            $Button = Find-SeElement -Driver $Driver -Wait -Timeout 5 -TagName "button"
+            # Click the element.
+            Invoke-SeClick -Element $Button
+            # Enter the email into the input field.
+            $EmailField = Find-SeElement -Driver $Driver -TagName "input"
+            Send-SeKeys -Element $EmailField -Keys $Email
+            # Find the new button element.
+            $Button = Find-SeElement -Driver $Driver -Wait -Timeout 5 -TagName "button"
+            # Click the new button element.
+            Invoke-SeClick -Element $Button
+            
+            # Prompt user to login to their email account. As Monzo sends them a "magic link".
+            # This "magic link" redirects them back to their chosen RedirectUri.
+            Write-Output -InputObject "Monzo has emailed you a 'magic link'.`nHead to your email account in the Selenium browser and click the magic link!"
+            # Fetch the URL if it matches.
+            do {
+                Write-Verbose -Message "Sleeping until redirect uri is detected..."
+                Start-Sleep -Seconds 1
+            } until ($Driver.Url -match "code=[^&]*")
+            
+            # Store the callback url.
+            [System.Uri]$CallbackUrl = $Driver.Url
+        }
+        catch {
+            # Stop the driver if an error occurs.
+            Stop-SeDriver -Driver $Driver
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
+    }
+
+    end {
+
+        try {
+            # $CallbackUrl matched successfully. Stop the driver.
+            Stop-SeDriver -Driver $Driver            
+            # Fetch authorisation code from $CallbackUrl
+            $AuthorisationCode = $CallbackUrl.Query.Trim("?code=").Split("&state=")[0]
+
+            # Populate PSCustom Object MonzoAPI.OAuth.AuthorisationCode.
+            [PSCustomObject]@{
+                PSTypeName           = "MonzoAPI.OAuth.AuthorisationCode"
+                AuthorisationCode    = $AuthorisationCode
+                MonzoApplication     = $MonzoApplication
+                FullCallbackUrl      = $CallbackUrl
+                AuthorisationBaseUrl = "https://auth.monzo.com/"
+                IssueDateTime        = (Get-Date)
+            }
+        }
+        catch {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
+    }
+}
