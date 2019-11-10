315ee91b5998989a9cda8552ff7112e51ce0540c
Dextroz <25749668+Dextroz@users.noreply.github.com>
2019-11-10 17:38:25 +0000

    Added explicit return. Fixed grammar. Edited switch. Edited comments.

--- pwsh-monzo/Public/New-MonzoTokens.ps1
+++ pwsh-monzo/Public/New-MonzoTokens.ps1
@@ -1,10 +1,10 @@
 function New-MonzoTokens {
     <#
     .SYNOPSIS
-        Obtain a new Monzo access token and refresh token.
+        Obtain a new Monzo access token or refresh token.
     
     .DESCRIPTION
-        Obtain a new Monzo access token and refresh token from the Monzo bank API.
+        Obtain a new Monzo access token or refresh token from the Monzo bank API.
     
     .PARAMETER MonzoAuthorisationCode
         A MonzoAPI.OAuth.AuthorisationCode PSCustomObject. Obtained from New-MonzoAuthorisationCode.
@@ -16,12 +16,12 @@ function New-MonzoTokens {
         A Monzo refresh token.
     
     .PARAMETER RefreshToken
-        A switch which redirects code flow for refreshing with a previous refresh token.
+        A switch which redirects code flow for obtaining a new access and refresh token with a previous refresh token.
     
     .EXAMPLE
         # Usage when getting tokens for the first time.
         $AuthorisationCode = New-MonzoAuthorisationCode -MonzoApplication $MonzoApplication -Email "foobar@somemail.com"
-        $MonzoTokens = New-MonzoTokens -MonzoAuthorisationCode $AuthorisationCode    
+        $MonzoTokens = New-MonzoTokens -MonzoAuthorisationCode $AuthorisationCode 
     
     .EXAMPLE
         # Usage when refreshing.
@@ -34,7 +34,7 @@ function New-MonzoTokens {
         $MonzoTokens = New-MonzoTokens -ClientCredential $Credentials -MonzoRefreshToken $MonzoRefreshToken -RefreshToken
     
     .NOTES
-        Use the parameters MonzoAuthorisationCode and MonzoRefreshToken when getting an access token and refresh token for the first time.
+        Use both MonzoAuthorisationCode and MonzoRefreshToken parameters when obtaining an access and refresh token for the first time.
         For refreshing, ensure you provide the -RefreshToken switch.
         https://docs.monzo.com/#acquire-an-access-token
     #>
@@ -58,7 +58,6 @@ function New-MonzoTokens {
         $MonzoRefreshToken,
 
         [Parameter(Mandatory = $false)]
-        [ValidateNotNullOrEmpty()]
         [Switch]
         $RefreshToken
     )
@@ -66,9 +65,9 @@ function New-MonzoTokens {
     begin {
 
         if ($MonzoAuthorisationCode) {
-            # Before doing anything, check that the PSCustomObject contains clientID, clientsecret and an auththorisation code.
+            # Before doing anything, check that the PSCustomObject contains clientID, client secret and an auththorisation code.
             if (-not ($MonzoAuthorisationCode.MonzoApplication.ClientCredential.UserName -and $MonzoAuthorisationCode.MonzoApplication.ClientCredential.Password -and $MonzoAuthorisationCode.AuthorisationCode)) {
-                Write-Error -Message "ClientId, clientsecret and authorisation code weren't present in MonzoAPI.Oauth.AuthorisationCode." -ErrorAction "Stop"
+                Write-Error -Message "ClientId, client secret and authorisation code weren't present in MonzoAPI.Oauth.AuthorisationCode." -ErrorAction "Stop"
             }
         }
     
@@ -79,29 +78,29 @@ function New-MonzoTokens {
         try {
             # Initalise request parameters.
             # If switch is or isn't supplied, set $RequestBody accordingly.
-            switch ($RefreshToken) {
+            $RequestBody = switch ($RefreshToken) {
                 $true {
                     Write-Verbose -Message "Refresh token flow."
-                    $RequestBody = @{
+                    @{
                         grant_type    = "refresh_token" 
                         client_id     = $ClientCredential.UserName
-                        client_secret = $ClientCredential.Password
+                        client_secret = $ClientCredential.GetNetworkCredential().Password
                         refresh_token = $MonzoRefreshToken
                     }
                 }
                 $false {
                     Write-Verbose -Message "Authorisation code flow."
-                    $RequestBody = @{
+                    @{
                         grant_type    = "authorization_code" 
                         client_id     = $MonzoAuthorisationCode.MonzoApplication.ClientCredential.UserName
-                        client_secret = $MonzoAuthorisationCode.MonzoApplication.ClientCredential.Password
+                        client_secret = $MonzoAuthorisationCode.MonzoApplication.ClientCredential.GetNetworkCredential().Password
                         redirect_uri  = $MonzoAuthorisationCode.MonzoApplication.RedirectUri
                         code          = $MonzoAuthorisationCode.AuthorisationCode
                     }
                 }
             }
             # Make POST request to Monzo API to retrieve an access and refresh token.
-            $Response = Invoke-RestMethod -Method "POST" -Uri "https://api.monzo.com/oauth2/token" -Body $RequestBody -Verbose:($PSBoundParameters["Verbose"] -eq $true)
+            $Response = Invoke-RestMethod -Method "POST" -Uri "https://api.monzo.com/oauth2/token" -Body $RequestBody -ContentType "application/x-www-form-urlencoded" -Verbose:($PSBoundParameters["Verbose"] -eq $true) -ErrorAction "Stop"
         }
         catch {
             $PSCmdlet.ThrowTerminatingError($PSItem)
@@ -111,7 +110,7 @@ function New-MonzoTokens {
     end {
 
         # Populate PSCustom Object MonzoAPI.OAuth.Tokens.
-        [PSCustomObject]@{
+        return [PSCustomObject]@{
             PSTypeName    = "MonzoAPI.OAuth.Tokens"
             AccessToken   = $Response.access_token
             RefreshToken  = $Response.refresh_token
