a41334f0b2bac02f223cb82a6a67b2b33fe49314
Dextroz <25749668+Dextroz@users.noreply.github.com>
2019-11-10 17:37:00 +0000

    Fixed spacing.

--- pwsh-monzo/Public/New-MonzoAuthorisationCode.ps1
+++ pwsh-monzo/Public/New-MonzoAuthorisationCode.ps1
@@ -46,7 +46,7 @@ function New-MonzoAuthorisationCode {
 
         # Before doing anything, check that the PSCustomObject contains clientID and clientSecret.
         if (-not ($MonzoApplication.ClientCredential.UserName -and $MonzoApplication.ClientCredential.Password)) {
-            Write-Error -Message "ClientId and clientsecret weren't present in MonzoAPI.Application." -ErrorAction "Stop"
+            Write-Error -Message "ClientId and client secret weren't present in MonzoAPI.Application." -ErrorAction "Stop"
         }
 
         # Check if Selenium-PowerShell is installed on the host.
